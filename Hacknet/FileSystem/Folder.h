05e17d3701a38cad88d2f7bd03f5c2d111f0b93b
Youen Corbel <tequiloutre@gmail.com>
2023-05-31 15:03:16 +0200

    Revert "Added CMake to use external libs"
    This reverts commit 9edcec8963950c2b707445af661c89feb0cf1214.

---
+++ Hacknet/FileSystem/Folder.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+
+class Folder
+{
+	std::string name = "Folder";
+	Folder* parentFolder = nullptr;
+	std::vector<Folder*> folders;
+	// std::vector<File*> files;
+
+public:
+
+	std::string GetName() const { return name; }
+	Folder* GetParentFolder() const { return parentFolder; }
+	std::vector<Folder*> GetFolders() const { return folders; }
+	Folder* GetFolderByName(const std::string& _folderName) const;
+	std::string GetPath() const;
+	
+	Folder(const std::string& _name);
+	~Folder();
+
+	void SetParentFolder(Folder* _parent);
+
+	bool ContainsFolder(const std::string& _folderName) const;
+	void AddFolder(Folder* _folder);
+};
---
+++ Hacknet/Network/WAN.cpp
@@ -0,0 +1,33 @@
+﻿#include "WAN.h"
+
+#include "System/VM.h"
+
+using namespace std;
+
+WAN::~WAN()
+{
+	for (const auto& _node : nodes)
+		delete _node.second;
+	nodes.clear();
+}
+
+bool WAN::GetNode(const string& _targetIP, Node** _outNode)
+{
+	if (!nodes.contains(_targetIP)) return false;
+	*_outNode = nodes[_targetIP];
+	return true;
+}
+
+void WAN::AddNode(Node* _node)
+{
+	const string _ip = _node->GetIP();
+	nodes.emplace(_ip, _node);
+	VM::Log("Added node : {}", _ip);
+}
+
+string WAN::GetRandomIP()
+{
+	string _newIp = to_string(rand() % 255) + "." + to_string(rand() % 255) + "." + to_string(rand() % 255) + "." + to_string(rand() % 255);
+	if (!nodes.contains(_newIp)) return _newIp;
+	return GetRandomIP();
+}
